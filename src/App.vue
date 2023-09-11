95e55bbba6e792809c1bcfe6cb77ec4f2c91b286
Robson de Moraes Silva <rms2307@outlook.com>
2023-09-10 23:07:44 -0300

    Fazendo primeira chamada para a API

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1 v-html="this.question"></h1>
+
+    <input type="radio" name="options" value="True" />
+    <label>True</label><br />
+
+    <input type="radio" name="options" value="False" />
+    <label>False</label><br />
+
+    <button class="send" type="button">Send</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  data() {
+    return {
+      question: undefined,
+      incorrectAnswers: undefined,
+      correctAnswers: undefined,
+    }
+  },
+  created() {
+    this.axios
+      .get("https://opentdb.com/api.php?amount=1&category=18")
+      .then((response) => {
+        this.question = response.data.results[0].question,
+        this.incorrectAnswers = response.data.results[0].incorrect_answers,
+        this.correctAnswers = response.data.results[0].correct_answer
+      })
+  },
 }
 </script>
 
@@ -21,6 +41,38 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+}
+
+h1 {
+  margin-top: 40px;
+}
+
+input[type="radio"] {
+  margin: 12px 4px;
+}
+
+button.send {
+  margin-top: 12px;
+  height: 40px;
+  min-width: 120px;
+  padding: 0 16px;
+  color: #fff;
+  background-color: #1867c0;
+  border: 1px solid #1867c0;
+  cursor: pointer;
+}
+
+section.score {
+  border-bottom: 1px solid black;
+  padding: 24px;
+  font-size: 18px;
+
+  span {
+    padding: 8px;
+    font-weight: bold;
+    border: 1px solid black;
+  }
 }
 </style>
